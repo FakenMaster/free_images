84041e916c4f5bda9e9409151d3a78269c7295cf
FaKenMaster <linchenpeng2020@qq.com>
2020-05-27 21:39:56 +0800

    video view widget
    video view widget

--- lib/ui/pixabay/pixabay_video_widget.dart
+++ lib/ui/pixabay/pixabay_video_widget.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/widgets.dart';
 
+class ViewVideoWidget extends StatefulWidget {
+  @override
+  _ViewVideoWidgetState createState() => _ViewVideoWidgetState();
+}
+
+class _ViewVideoWidgetState extends State<ViewVideoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
